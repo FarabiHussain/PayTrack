70326e446bb3026fe17fef03d854620dbd06e9cb
Farabi Hussain <farabihussain8@gmail.com>
2024-03-05 19:44:42 -0600

    logic done

---
+++ doc_utils.py
@@ -0,0 +1,76 @@
+from docx.oxml import OxmlElement
+from docx.oxml.ns import qn
+from docx.text.paragraph import Paragraph
+from docx.table import _Cell
+
+
+
+def set_cell_color(cell, color="ffffff"):
+    cell_xml = cell._tc
+    cell_props = cell_xml.get_or_add_tcPr()
+    shade_obj = OxmlElement("w:shd")
+    shade_obj.set(qn("w:fill"), color)
+    cell_props.append(shade_obj)
+
+
+def make_rows_bold(*rows):
+    for row in rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                for run in paragraph.runs:
+                    run.font.bold = True
+
+
+def set_row_color(row, color="FFFFFF"):
+    for cell in row.cells:
+        set_cell_color(cell, color)
+
+
+def insert_paragraph_after(paragraph, text=None, style=None):
+    new_p = OxmlElement("w:p")
+    paragraph._p.addnext(new_p)
+    new_para = Paragraph(new_p, paragraph._parent)
+    if text:
+        new_para.add_run(text)
+    if style is not None:
+        new_para.style = style
+    return new_para
+
+
+def set_cell_border(cell: _Cell, **kwargs):
+    """
+    Set cell`s border
+    Usage:
+    set_cell_border(
+        cell,
+        top={"sz": 12, "val": "single", "color": "#FF0000", "space": "0"},
+        bottom={"sz": 12, "color": "#00FF00", "val": "single"},
+        start={"sz": 24, "val": "dashed", "shadow": "true"},
+        end={"sz": 12, "val": "dashed"},
+    )
+    """
+    tc = cell._tc
+    tcPr = tc.get_or_add_tcPr()
+
+    # check for tag existance, if none found, then create one
+    tcBorders = tcPr.first_child_found_in("w:tcBorders")
+    if tcBorders is None:
+        tcBorders = OxmlElement("w:tcBorders")
+        tcPr.append(tcBorders)
+
+    # list over all available tags
+    for edge in ("start", "top", "end", "bottom", "insideH", "insideV"):
+        edge_data = kwargs.get(edge)
+        if edge_data:
+            tag = "w:{}".format(edge)
+
+            # check for tag existnace, if none found, then create one
+            element = tcBorders.find(qn(tag))
+            if element is None:
+                element = OxmlElement(tag)
+                tcBorders.append(element)
+
+            # looks like order of attributes is important
+            for key in ["sz", "val", "color", "space", "shadow"]:
+                if key in edge_data:
+                    element.set(qn("w:{}".format(key)), str(edge_data[key]))
--- main.py
+++ main.py
@@ -1,75 +1,111 @@
-# Import docx NOT python-docx 
-import docx, os
-from docx.enum.table import WD_ALIGN_VERTICAL, WD_TABLE_ALIGNMENT
+import docx, os, datetime
+from doc_utils import *
+from data_utils import *
 from docx.shared import Cm as CM
 from docx.oxml import OxmlElement
 from docx.oxml.ns import qn
+from docx.text.paragraph import Paragraph
 
-os.system('cls')
+os.system("cls")
+timestamp = str(datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%I"))
+client = 'Awais Saleem'
 
-def set_cell_color(cell, color='ffffff'):
-    cell_xml = cell._tc
-    cell_props = cell_xml.get_or_add_tcPr()
-    shade_obj = OxmlElement('w:shd')
-    shade_obj.set(qn('w:fill'), color)
-    cell_props.append(shade_obj)
+# Create an instance of a word document
+doc = docx.Document("invoice.docx")
 
-# Create an instance of a word document 
-doc = docx.Document() 
+insert_paragraph_after(
+    doc.add_paragraph(), 
+    (
+        "Invoice#:\t [invoice id]"
+        + "\nDate:\t\t " + timestamp
+        + "\nInvoiced to:\t " + client
+        + "\n"
+    )
+)
 
-# Add a Title to the document 
-doc.add_heading('GeeksForGeeks', 0) 
-
-# Table data in a form of list 
-data = [
-    {'desc': 'Notary', 'qty': 2, 'amount': 30},
-    {'desc': 'Immigration Services', 'qty': 1, 'amount': 500},
-    {'desc': 'Government Fees', 'qty': 1, 'amount': 50},
+# Table data in a form of list
+columns = [
+    {'heading': 'SL.', 'width': 1.0},
+    {'heading': 'DESCRIPTION', 'width': 20.0},
+    {'heading': 'QTY', 'width': 1.0},
+    {'heading': 'RATE', 'width': 1.0},
+    {'heading': 'AMOUNT', 'width': 1.0},
 ]
 
-# Creating a table object 
-table = doc.add_table(rows=1, cols=5) 
+# Table data in a form of list
+data = [
+    {"desc": "Notary", "qty": 2, "rate": 30, "gst": 0.05, 'pst': 0.07},
+    {"desc": "Immigration Services", "qty": 1, "rate": 500, "gst": 0.05, 'pst': 0.07},
+    {"desc": "Government Fees", "qty": 1, "rate": 50, "gst": 0.0, 'pst': 0.0},
+]
 
-# Adding heading in the 1st row of the table 
-row = table.rows[0].cells 
-row[0].text = 'SL.'
-row[1].text = 'DESC'
-row[2].text = 'QTY'
-row[3].text = 'AMOUNT'
-row[4].text = 'TOTAL'
+####################################################################################
 
-# Adding data from the list to the table 
-for index, entry in enumerate(data): 
+# Creating a table object
+items_table = doc.add_table(rows=1, cols=5)
 
-    # Adding a row and then adding data in it. 
-    row = table.add_row().cells
+# Adding heading in the 1st row of the table
+row = items_table.rows[0].cells
+for idx, col in enumerate(columns):
+    row[idx].text = col['heading']
 
-    # Converting id to string as table can only take string input 
+# Adding data from the list to the table
+for index, entry in enumerate(data):
+
+    # Adding a row and then adding data in it.
+    row = items_table.add_row().cells
+
     row[0].text = str(index)
-    row[1].text = str(entry['desc'])
-    row[2].text = str(entry['qty'])
-    row[3].text = str(entry['amount'])
-    row[4].text = str(entry['qty'] * entry['amount'])
+    row[1].text = str(entry["desc"])
+    row[2].text = str(entry["qty"])
+    row[3].text = str(entry["rate"])
+    row[4].text = str(entry["qty"] * entry["rate"])
+
+for cell in items_table.rows[0].cells:
+    set_cell_border(cell, bottom={"sz": 6, "color": "#AAAAAA", "val": "single", "space": "10"})
+    set_cell_border(cell, top={"sz": 6, "color": "#AAAAAA", "val": "single", "space": "15"})
 
 
-for cell in table.columns[0].cells: cell.width = CM(1.0)
-for cell in table.columns[1].cells: cell.width = CM(9.9)
-for cell in table.columns[2].cells: cell.width = CM(1.0)
-for cell in table.columns[3].cells: cell.width = CM(1.0)
-for cell in table.columns[4].cells: cell.width = CM(1.0)
+# set column widths
+for idx, col in enumerate(columns):
+    for index, cell in enumerate(items_table.columns[idx].cells):
+        cell.width = CM(col['width'])
 
-for index, row in enumerate(table.rows):
-    row.height = CM(0.5)
-    row_color = 'E5E4E2' if index%2 == 0 else 'FFFFFF'
+        if (index > 0):
+            set_cell_border(cell, bottom={"sz": 6, "color": "#EEEEEE", "val": "single", "space": "8"})
 
-    for cell in row.cells:
-        set_cell_color(cell, row_color)
-        # cell.vertical_alignment = WD_ALIGN_VERTICAL.CENTER
+    set_cell_border(cell, bottom={"sz": 6, "color": "#AAAAAA", "val": "single", "space": "0"})
 
-# table.cell(0, 0).vertical_alignment = WD_ALIGN_VERTICAL.BOTTOM
-table.alignment = WD_TABLE_ALIGNMENT.RIGHT
 
-# Now save the document to a location 
-doc.save('test.docx')
-os.startfile('test.docx')
+# set row heights
+for index, row in enumerate(items_table.rows):
+    row.height = CM(1)
 
+make_rows_bold(items_table.rows[0])
+
+#####################################################################################
+
+total_table = doc.add_table(rows=1, cols=5)
+res = calculate_totals(data)
+
+total_table_data = [
+    [str('GST @5%'), '','','', str(res['gst'])],
+    [str('PST @7%'), '','','', str(res['pst'])],
+    [str('TOTAL'), '','','', str(res['dollar'])],
+]
+
+for total_row in total_table_data:
+
+    row = total_table.add_row().cells
+
+    for idx, col in enumerate(total_row):
+        row[idx].text = col 
+
+    make_rows_bold(total_table.rows[-1])
+    # items_table.cell(-1,-0).merge(items_table.cell(-1,1))
+
+
+#####################################################################################
+
+doc.save("invoice-test.docx")
+os.startfile("invoice-test.docx")
